492c8e1b17e72dbc2be7007d808aeff1c85c4702
Aidan Gao <aidangao345@gmail.com>
2024-07-03 16:17:43 -0400

    added spatial data

--- DAEGC/utils.py
+++ DAEGC/utils.py
@@ -1,13 +1,34 @@
 import numpy as np
 import torch
 from sklearn.preprocessing import normalize
-
+from torch_geometric.data import Data
 from torch_geometric.datasets import Planetoid
 
 
 def get_dataset(dataset):
-    datasets = Planetoid('./dataset', dataset)
+    create_pyg_dataset()
+    #datasets = Planetoid('./dataset', dataset)
+    datasets = create_pyg_dataset('dataset.npy','edge_index.npy','edge_weight.npy', 'labels.npy')
     return datasets
+
+
+def create_pyg_dataset(node_features_file, edge_index_file, edge_weight_file, label_file):
+    # Load numpy arrays from files
+    node_features = np.load(node_features_file)
+    edge_index = np.load(edge_index_file)
+    edge_weight = np.load(edge_weight_file)
+    labels = np.load(label_file)
+    # Convert numpy arrays to torch tensors
+    x = torch.tensor(node_features, dtype=torch.float)
+    edge_index = torch.tensor(edge_index, dtype=torch.long)
+    edge_weight = torch.tensor(edge_weight, dtype=torch.float)
+    y = torch.tensor(labels, dtype=torch.long) 
+    # Create a PyTorch Geometric data object
+    data = Data(x=x, edge_index=edge_index, edge_attr=edge_weight,y=y)
+    
+    return data
+
+
 
 def data_preprocessing(dataset):
     dataset.adj = torch.sparse_coo_tensor(
